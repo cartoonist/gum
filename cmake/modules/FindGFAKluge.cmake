f8d944bf8568bdc8f73bade821e69a35d19a7c50
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2020-02-24 11:34:33 +0100

    Fix bugs in CMake find modules
    The find modules are case-sensitive regarding package names.

--- cmake/modules/FindGFAKluge.cmake
+++ cmake/modules/FindGFAKluge.cmake
@@ -1,29 +1,29 @@
 # Find 'GFAKluge' library.
 #
 # This set the following variables:
-#   - GFAKLUGE_FOUND
-#   - GFAKLUGE_INCLUDE_DIRS
+#   - GFAKluge_FOUND
+#   - GFAKluge_INCLUDE_DIRS
 #
 # and the following imported targets:
 #   - GFAKluge::GFAKluge
 
-if(GFAKLUGE_INCLUDE_DIRS)
-  set(GFAKLUGE_FIND_QUIETLY TRUE)
+if(GFAKluge_INCLUDE_DIRS)
+  set(GFAKluge_FIND_QUIETLY TRUE)
 else()
-  # If GFAKLUGE_INCLUDE_DIRS is not set, this searches for the header file.
-  find_path(GFAKLUGE_INCLUDE_DIRS gfakluge.hpp)
-endif(GFAKLUGE_INCLUDE_DIRS)
+  # If GFAKluge_INCLUDE_DIRS is not set, this searches for the header file.
+  find_path(GFAKluge_INCLUDE_DIRS gfakluge.hpp)
+endif(GFAKluge_INCLUDE_DIRS)
 
-# handle the QUIETLY and REQUIRED arguments and set GFAKLUGE_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set GFAKluge_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GFAKluge DEFAULT_MSG GFAKLUGE_INCLUDE_DIRS)
+find_package_handle_standard_args(GFAKluge DEFAULT_MSG GFAKluge_INCLUDE_DIRS)
 
-mark_as_advanced(GFAKLUGE_INCLUDE_DIRS)
+mark_as_advanced(GFAKluge_INCLUDE_DIRS)
 
 # Define `GFAKluge::GFAKluge` imported target
-if(GFAKLUGE_FOUND AND NOT TARGET GFAKluge::GFAKluge)
+if(GFAKluge_FOUND AND NOT TARGET GFAKluge::GFAKluge)
   add_library(GFAKluge::GFAKluge INTERFACE IMPORTED)
   set_target_properties(GFAKluge::GFAKluge PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${GFAKLUGE_INCLUDE_DIRS}")
+    INTERFACE_INCLUDE_DIRECTORIES "${GFAKluge_INCLUDE_DIRS}")
 endif()
--- cmake/modules/FindSparseHash.cmake
+++ cmake/modules/FindSparseHash.cmake
@@ -1,33 +1,33 @@
 # Find 'libsparsehash' library.
 #
 # This set the following variables:
-#   - SPARSEHASH_FOUND
-#   - SPARSEHASH_VERSION
-#   - SPARSEHASH_INCLUDE_DIRS
+#   - SparseHash_FOUND
+#   - SparseHash_VERSION
+#   - SparseHash_INCLUDE_DIRS
 #
 # and the following imported targets:
 #   - SparseHash::SparseHash
 
-if(SPARSEHASH_INCLUDE_DIRS)
-  set(SPARSEHASH_FIND_QUIETLY TRUE)
+if(SparseHash_INCLUDE_DIRS)
+  set(SparseHash_FIND_QUIETLY TRUE)
 else()
   # Try pkg-config, first.
   find_package(PkgConfig QUIET)
-  pkg_check_modules(SPARSEHASH QUIET libsparsehash>=2.0.2)
-  # If SPARSEHASH_INCLUDE_DIRS is not set, this searches for the header file.
-  find_path(SPARSEHASH_INCLUDE_DIRS sparsehash/sparse_hash_map)
-endif(SPARSEHASH_INCLUDE_DIRS)
+  pkg_check_modules(SparseHash QUIET libsparsehash>=2.0.2)
+  # If SparseHash_INCLUDE_DIRS is not set, this searches for the header file.
+  find_path(SparseHash_INCLUDE_DIRS sparsehash/sparse_hash_map)
+endif(SparseHash_INCLUDE_DIRS)
 
-## handle the QUIETLY and REQUIRED arguments and set SPARSEHASH_FOUND to TRUE if
+## handle the QUIETLY and REQUIRED arguments and set SparseHash_FOUND to TRUE if
 ## all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libsparsehash DEFAULT_MSG SPARSEHASH_INCLUDE_DIRS)
+find_package_handle_standard_args(SparseHash DEFAULT_MSG SparseHash_INCLUDE_DIRS)
 
-mark_as_advanced(SPARSEHASH_FOUND SPARSEHASH_VERSION SPARSEHASH_INCLUDE_DIRS)
+mark_as_advanced(SparseHash_FOUND SparseHash_VERSION SparseHash_INCLUDE_DIRS)
 
 # Define `SparseHash::SparseHash` imported target
-if(SPARSEHASH_FOUND AND NOT TARGET SparseHash::SparseHash)
+if(SparseHash_FOUND AND NOT TARGET SparseHash::SparseHash)
   add_library(SparseHash::SparseHash INTERFACE IMPORTED)
   set_target_properties(SparseHash::SparseHash PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${SPARSEHASH_INCLUDE_DIRS}")
+    INTERFACE_INCLUDE_DIRECTORIES "${SparseHash_INCLUDE_DIRS}")
 endif()
