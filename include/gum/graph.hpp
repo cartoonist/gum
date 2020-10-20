ceee18fc6c7d7e7ef351df68775e2977c2d512fa
Ali Ghaffaari <ali.ghaffaari@hhu.de>
2020-10-20 21:02:18 +0200

    Define graph generic header file

---
+++ include/gum/graph.hpp
@@ -0,0 +1,25 @@
+/**
+ *    @file  graph.hpp
+ *   @brief  Graph generic header file
+ *
+ *  This is the highest-level header file aggregates all modules defining graph data
+ *  structures.
+ *
+ *  @author  Ali Ghaffaari (\@cartoonist), <ali.ghaffaari@mpi-inf.mpg.de>
+ *
+ *  @internal
+ *       Created:  Tue Oct 20, 2020  20:55
+ *  Organization:  Max-Planck-Institut fuer Informatik
+ *     Copyright:  Copyright (c) 2020, Ali Ghaffaari
+ *
+ *  This source code is released under the terms of the MIT License.
+ *  See LICENSE file for more information.
+ */
+
+#ifndef  GUM_GRAPH_HPP__
+#define  GUM_GRAPH_HPP__
+
+#include "seqgraph.hpp"
+#include "seqgraph_interface.hpp"
+
+#endif  /* --- #ifndef GUM_GRAPH_HPP__ --- */
