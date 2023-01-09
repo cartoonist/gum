4392f76e3e4e396064e22a227b3bacfdcd0aba45
Ali Ghaffaari <ali.ghaffaari@uni-bielefeld.de>
2023-01-09 16:15:32 +0100

    Embed the last bundle dependency: ParallelHashmap

--- ext/external.cmake
+++ ext/external.cmake
@@ -4,7 +4,7 @@ include(ExternalProject)
 
 # Include GFAKluge in the source code
 if(NOT EXISTS ${PROJECT_SOURCE_DIR}/include/gum/internal/gfakluge.hpp)
-  message(STATUS "Using bundled GFAKluge library internally")
+  message(STATUS "Embedding GFAKluge library")
   file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/include/gum/internal)
   set(GFAKluge_SOURCE_DIR ${PROJECT_SOURCE_DIR}/ext/gfakluge)
   ExternalProject_Add(gfakluge_git
@@ -19,7 +19,7 @@ endif()
 
 # Include `xxsds/sdsl-lite` in the source code
 if(NOT EXISTS ${PROJECT_SOURCE_DIR}/include/gum/internal/sdsl/config.hpp)
-  message(STATUS "Using bundled xxsds/sdsl-lite library internally")
+  message(STATUS "Embedding sdsl-lite library")
   file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/include/gum/internal/sdsl)
   set(SDSL_SOURCE_DIR ${PROJECT_SOURCE_DIR}/ext/sdsl-lite)
   ExternalProject_Add(sdsl_git
@@ -31,25 +31,15 @@ if(NOT EXISTS ${PROJECT_SOURCE_DIR}/include/gum/internal/sdsl/config.hpp)
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${SDSL_SOURCE_DIR}/include/sdsl/ ${PROJECT_SOURCE_DIR}/include/gum/internal/sdsl)
 endif()
 
-if(NOT TARGET ParallelHashmap::ParallelHashmap)
-  if(NOT USE_BUNDLED_PARALLEL_HASHMAP)
-    message(FATAL_ERROR "Parallel Hashmap library not found. "
-      "Pass in `-DUSE_BUNDLED_PARALLEL_HASHMAP=on` when running cmake to use the bundled version. "
-      "It will be installed alongside the library.")
-  endif()
-  message(STATUS "Using bundled Parallel Hashmap library")
+if(NOT EXISTS ${PROJECT_SOURCE_DIR}/include/gum/internal/parallel_hashmap/phmap.h)
+  message(STATUS "Embedding Parallel Hashmap library")
+  file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/include/gum/internal/parallel_hashmap)
   set(ParallelHashmap_SOURCE_DIR ${PROJECT_SOURCE_DIR}/ext/parallel-hashmap)
   ExternalProject_Add(parallelhashmap_git
     DOWNLOAD_COMMAND git -C ${PROJECT_SOURCE_DIR} submodule update --init --recursive -- ${ParallelHashmap_SOURCE_DIR}
     SOURCE_DIR ${ParallelHashmap_SOURCE_DIR}
-    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>")
-  ExternalProject_Get_Property(parallelhashmap_git INSTALL_DIR)
-  add_library(ParallelHashmap::ParallelHashmap INTERFACE IMPORTED)
-  set_target_properties(ParallelHashmap::ParallelHashmap PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "$<BUILD_INTERFACE:${INSTALL_DIR}/include>;$<INSTALL_INTERFACE:$<INSTALL_PREFIX>/${CMAKE_INSTALL_INCLUDEDIR}>")
-  install(DIRECTORY ${INSTALL_DIR}/include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo "Skipping configure step..."
+    BUILD_COMMAND ${CMAKE_COMMAND} -E echo "Skipping build step..."
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E echo "Skipping install step..."
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${ParallelHashmap_SOURCE_DIR}/parallel_hashmap/ ${PROJECT_SOURCE_DIR}/include/gum/internal/parallel_hashmap)
 endif()
-
-if(ParallelHashmap_FOUND)
-  set(ParallelHashmap_PKG_CFLAGS "-I${ParallelHashmap_INCLUDE_DIRS}")
-endif(ParallelHashmap_FOUND)
